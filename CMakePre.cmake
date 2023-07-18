5ff3d58861da6edb2547ae340557c2f114a6ddde
weenchvd <d88744@gmail.com>
2023-07-18 23:11:24 +0300

    Change 005

--- CMakePre.cmake
+++ CMakePre.cmake
@@ -1,11 +1,16 @@
+set(MES_PREF_NOTICE "** [NOTICE] ** ")
+set(MES_PREF_INFO   "** [INFO] ** ")
+set(MES_PREF_VARS   "** [VARS] ** ")
+
+
 if (WIN32 AND ${CMAKE_C_COMPILER} STREQUAL "cl" AND ${CMAKE_CXX_COMPILER} STREQUAL "cl")
-  message(">> [NOTICE] If you get the error message \"Compiler not found\", run the CMake executable after running \"vcvarsall.bat\".")
+  message(${MES_PREF_NOTICE} "If you get the error message \"Compiler not found\", run the CMake executable after running \"vcvarsall.bat\".")
   file(GLOB_RECURSE VCVARSALL_LIST "C:/Program Files*/vcvarsall.bat")
   list(LENGTH VCVARSALL_LIST VCVARSALL_LIST_LENGTH)
   if(${VCVARSALL_LIST_LENGTH} GREATER 0)
-    message(">> [NOTICE] \"vcvarsall.bat\": \"${VCVARSALL_LIST}\"")
+    message(${MES_PREF_INFO} "Path of \"vcvarsall.bat\" file: \"${VCVARSALL_LIST}\"")
   else()
-    message(">> [NOTICE] \"vcvarsall.bat\" not found")
+    message(${MES_PREF_INFO} "File \"vcvarsall.bat\" not found")
   endif()
 endif()
 
